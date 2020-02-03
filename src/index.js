c08515b0ffbe221e075ef51611dac87203c1bc9b
Jayro_Salazar <megajjks@gmail.com>
2020-02-03 12:00:35 -0600

    ConfiguracióÂon de nuestro entorno de trabajo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App'
+import './global.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('app');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ReactDOM.render(__qué__, __dónde__);
+ReactDOM.render(<App/>, container);
